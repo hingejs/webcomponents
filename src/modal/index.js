805a830e00d768b977c6771fb51406feca321ed0
gforti <gforti@its.jnj.com>
2019-09-07 16:19:18 -0400

    including the modal

--- src/modal/index.js
+++ src/modal/index.js
@@ -1,8 +1,2 @@
-/**
- * Copyright: Copyright © 2019
- * This file contains trade secrets of Johnson & Johnson. No part may be reproduced or transmitted in any
- * form by any means or for any purpose without the express written permission of Johnson & Johnson.
- */
-
 import './modal-blur.js'
 import './modal-popup.js'
